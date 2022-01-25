a63bc520497bfa37f712f5652f778e37230b9685
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-25 22:51:30 +0300

    Fixed Printing

--- result.m
+++ result.m
@@ -24,8 +24,8 @@ function self = result(given, material, C, d)
 end
 function N_t_min = find_yield_limit(self)
 	K_s = 1+0.5/self.C;
-	S_ut = material.find_strength(self.d);
-	S_ys = S_ut*given.S_ys_percentage;
+	S_ut = self.material.find_strength(self.d);
+	S_ys = S_ut*self.given.S_ys_percentage;
 	N_a_min = K_s*1.1*self.given.delta_max*self.given.G*self.given.SF_yield_min/pi/self.d/self.C^2/S_ys;
 	N_t_min = N_a_min+2;
 end
@@ -43,7 +43,7 @@ end
 function print(self)
 	D = self.C*self.d;
 	N_a = self.N_t-2;
-	k = given.G*self.d/8/self.C^3/N_a;
+	k = self.given.G*self.d/8/self.C^3/N_a;
 	L_s = (self.N_t+1)*self.d;
 	L_f = L_s+1.1*self.given.delta_max;
 	F_s = k*1.1*self.given.delta_max;
@@ -59,13 +59,13 @@ function print(self)
 	m = pi*self.d^2/4*pi*D*N_a*self.given.rho;
 	f_n = pi*sqrt(k/self.given.M);
 	SF_surge = f_n/self.given.f;
-	S_ut = material.find_strength(self.d);
-	S_us = S_ut*given.S_us_percentage;
-	S_y = S_ut*given.S_y_percentage;
-	S_ys = S_ut*given.S_ys_percentage;
+	S_ut = self.material.find_strength(self.d);
+	S_us = S_ut*self.given.S_us_percentage;
+	S_y = S_ut*self.given.S_y_percentage;
+	S_ys = S_ut*self.given.S_ys_percentage;
 	tau_s = K_s*8*F_s*self.C/pi/self.d^2;
 	SF_yield = S_ys/tau_s;
-	S_s = S_ut*given.S_s_percentage;
+	S_s = S_ut*self.given.S_s_percentage;
 	file = printer('ME424_Project3_Gecgel_Sipahioglu.txt');
 	file.print('F_min = %g N', self.given.F_min);
 	file.print('F_max = %g N', self.given.F_max);
@@ -75,7 +75,7 @@ function print(self)
 	file.print('delta_cl = %g m', 0.1*self.given.delta_max);
 	file.print('k = %g N/m', k);
 	file.print('N_a = %g', N_a);
-	file.print('N_t = %g', N_t);
+	file.print('N_t = %g', self.N_t);
 	file.print('L_s = %g m', L_s);
 	file.print('L_f = %g m', L_f);
 	file.print('F_s = %g N', F_s);
@@ -94,6 +94,7 @@ function print(self)
 	file.print('tau_s = %g Pa', tau_s);
 	file.print('SF_yield = %g', SF_yield);
 	file.print('S_s = %g Pa', S_s);
+	file.print('cost = %g ₺', self.cost);
 end
 end
 end
