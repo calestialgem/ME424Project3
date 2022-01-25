9c3a75eec4f5d2ae3c0299d0a2ac088f71290380
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-25 22:18:39 +0300

    Surge Limit and Min Coil Count Implemented

--- result.m
+++ result.m
@@ -16,6 +16,7 @@ properties
 	% lambda_eff
 	% delta_cr
 	% SF_buckling
+	cost
 end
 methods
 function self = result(given, material, C, d)
@@ -31,23 +32,30 @@ function self = result(given, material, C, d)
 	self.S_y = self.S_ut*given.S_y_percentage;
 	self.S_ys = self.S_ut*given.S_ys_percentage;
 	self.S_s = self.S_ut*given.S_s_percentage;
+	N_t_max = min(self.find_surge_limit(), self.find_buckling_limit());
+	N_t_min = self.find_yield_limit();
+	if N_t_min > N_t_max
+		cost = inf;
+		return
+	end
+	cost = material.find_cost(d, N_t_min, self.D);
 	% lambda_eff = 0.5*L_f/D;
 	% delta_cr = L_f*c_1*(1-sqrt(1-c_2/lambda_eff^2));
 	% SF_buckling = delta_cr/delta_max;
 end
-function N_a_min = find_yield_limit(self)
+function N_t_min = find_yield_limit(self)
 	N_a_min = self.K_s*1.1*self.given.delta_max*self.given.G*self.given.SF_yield_min/pi/self.d/self.C^2/self.given.S_ys;
+	N_t_min = N_a_min+2;
 end
-function N_a = find_surge_limit(self)
+function N_t_max = find_surge_limit(self)
+	N_a_max = self.given.G*self.d*pi^2/8/self.C^3/self.given.M/self.given.SF_surge_min^2*self.given.f^2;
+	N_t_max = N_a_max+2;
 end
-function N_a = find_fatigue_limit(self)
-end
-function N_a_max = find_buckling_limit(self)
+function N_t_max = find_buckling_limit(self)
 	delta_cr_min = self.given.SF_buckling_min*self.given.delta_max;
 	L_f_max = max(self.given.c_1*self.given.c_2*self.D^2/2/delta_cr_min/self.given.alpha+delta_cr_min/2/self.given.c_1, sqrt(self.given.c_2)*self.D/self.given.alpha);
 	L_s_max = L_f_max-1.1*self.given.delta_max;
 	N_t_max = L_s_max/self.d-1;
-	N_a_max = N_t_max-2;
 end
 function print(self)
 	file = printer('ME424_Project3_Gecgel_Sipahioglu.txt');
