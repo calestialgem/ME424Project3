a293ae2f226b335fb1fd2c61cd07c20fcd9e0570
Cem GEÇGEL <aa-albara@hotmail.com>
2022-01-26 11:39:54 +0300

    Refactored Duplicated Code Away

--- result.m
+++ result.m
@@ -4,27 +4,50 @@ properties
 	material material
 	C
 	d
+	D
+	S_ut
+	S_us
+	S_y
+	S_ys
+	S_s
+	K_s
+	K_w
+	k
 	N_t
+	N_a
+	L_s
+	L_f
+	F_s
+	SF_buckling
+	m
+	f_n
+	SF_surge
+	tau_s
+	SF_yield
+	tau_a
+	tau_m
+	SF_fatigue
 	cost
 end
 methods
 function self = result(given, material, C, d)
+	% Parameters
 	self.given = given;
 	self.material = material;
 	self.C = C;
 	self.d = d;
-	S_ut = self.material.find_strength(self.d);
-	S_us = S_ut*self.given.S_us_percentage;
-	S_s = S_ut*self.given.S_s_percentage;
-	K_w = (4*C-1)/(4*C-4)+0.615/C;
-	tau_a = K_w*8*(given.F_max-given.F_min)/2*C/pi/d^2;
-	tau_m = K_w*8*abs(given.F_max+given.F_min)*C/pi/d^2;
-	S_f = S_s/2/(1-S_s/2/S_us);
-	SF_fatigue = 1/(tau_a/S_f+tau_m/S_us);
-	if SF_fatigue < given.SF_fatigue_min
-		self.cost = inf;
-		return;
-	end
+
+	% Things that only depend on the parameters.
+	self.D = self.C*self.d;
+	self.S_ut = self.material.find_strength(self.d);
+	self.S_us = self.S_ut*self.given.S_us_percentage;
+	self.S_y = self.S_ut*self.given.S_y_percentage;
+	self.S_ys = self.S_ut*self.given.S_ys_percentage;
+	self.S_s = self.S_ut*self.given.S_s_percentage;
+	self.K_s = 1+0.5/self.C;
+	self.K_w = (4*self.C-1)/(4*self.C-4)+0.615/self.C;
+
+	% Find the amount of coils.
 	N_t_max = min(self.find_surge_limit(), self.find_buckling_limit());
 	N_t_min = self.find_yield_limit();
 	if N_t_min > N_t_max
@@ -32,13 +55,47 @@ function self = result(given, material, C, d)
 		return
 	end
 	self.N_t = N_t_min;
-	self.cost = material.find_cost(d, self.N_t, C*d);
+
+	% Things that depend on the amount of coils.
+	self.N_a = self.N_t-2;
+	self.k = self.given.G*self.d/8/self.C^3/self.N_a;
+	self.L_s = (self.N_t+1)*self.d;
+	self.L_f = self.L_s+1.1*self.given.delta_max;
+
+	% Buckling analysis.
+	lambda_eff = 0.5*self.L_f/self.D;
+	if self.given.c_2/lambda_eff^2 <= 1
+		delta_cr = self.L_f*self.given.c_1*(1-sqrt(1-self.given.c_2/lambda_eff^2));
+		self.SF_buckling = delta_cr/self.given.delta_max;
+	else
+		self.SF_buckling = inf;
+	end
+
+	% Surge analysis.
+	self.m = pi*self.d^2/4*pi*self.D*self.N_a*self.given.rho;
+	self.f_n = pi*sqrt(self.k/self.given.M);
+	self.SF_surge = self.f_n/self.given.f;
+
+	% Yield analysis.
+	self.F_s = self.k*1.1*self.given.delta_max;
+	self.tau_s = self.K_s*8*self.F_s*self.C/pi/self.d^2;
+	self.SF_yield = self.S_ys/self.tau_s;
+
+	% Fatigue analysis.
+	self.tau_a = self.K_w*8*(self.given.F_max-self.given.F_min)/2*self.C/pi/self.d^2;
+	self.tau_m = self.K_w*8*abs(self.given.F_max+self.given.F_min)*self.C/pi/self.d^2;
+	S_f = self.S_s/2/(1-self.S_s/2/self.S_us);
+	self.SF_fatigue = 1/(self.tau_a/S_f+self.tau_m/self.S_us);
+	if self.SF_fatigue < given.SF_fatigue_min
+		self.cost = inf;
+		return;
+	end
+
+	% Find the cost if we reach here, the cost in infinite otherwise.
+	self.cost = material.find_cost(self.d, self.N_t, self.D);
 end
 function N_t_min = find_yield_limit(self)
-	K_s = 1+0.5/self.C;
-	S_ut = self.material.find_strength(self.d);
-	S_ys = S_ut*self.given.S_ys_percentage;
-	N_a_min = K_s*1.1*self.given.delta_max*self.given.G*self.given.SF_yield_min/pi/self.d/self.C^2/S_ys;
+	N_a_min = self.K_s*1.1*self.given.delta_max*self.given.G*self.given.SF_yield_min/pi/self.d/self.C^2/self.S_ys;
 	N_t_min = N_a_min+2;
 end
 function N_t_max = find_surge_limit(self)
@@ -46,73 +103,43 @@ function N_t_max = find_surge_limit(self)
 	N_t_max = N_a_max+2;
 end
 function N_t_max = find_buckling_limit(self)
-	D = self.C*self.d;
 	delta_cr_min = self.given.SF_buckling_min*self.given.delta_max;
-	L_f_max = max(self.given.c_1*self.given.c_2*D^2/2/delta_cr_min/self.given.alpha+delta_cr_min/2/self.given.c_1, sqrt(self.given.c_2)*D/self.given.alpha);
+	L_f_max = max(self.given.c_1*self.given.c_2*self.D^2/2/delta_cr_min/self.given.alpha+delta_cr_min/2/self.given.c_1, sqrt(self.given.c_2)*self.D/self.given.alpha);
 	L_s_max = L_f_max-1.1*self.given.delta_max;
 	N_t_max = L_s_max/self.d-1;
 end
 function print(self)
-	D = self.C*self.d;
-	N_a = self.N_t-2;
-	k = self.given.G*self.d/8/self.C^3/N_a;
-	L_s = (self.N_t+1)*self.d;
-	L_f = L_s+1.1*self.given.delta_max;
-	F_s = k*1.1*self.given.delta_max;
-	K_s = 1+0.5/self.C;
-	K_w = (4*self.C-1)/(4*self.C-4)+0.615/self.C;
-	lambda_eff = 0.5*L_f/D;
-	if self.given.c_2/lambda_eff^2 <= 1
-		delta_cr = L_f*self.given.c_1*(1-sqrt(1-self.given.c_2/lambda_eff^2));
-		SF_buckling = delta_cr/self.given.delta_max;
-	else
-		SF_buckling = inf;
-	end
-	m = pi*self.d^2/4*pi*D*N_a*self.given.rho;
-	f_n = pi*sqrt(k/self.given.M);
-	SF_surge = f_n/self.given.f;
-	S_ut = self.material.find_strength(self.d);
-	S_us = S_ut*self.given.S_us_percentage;
-	S_y = S_ut*self.given.S_y_percentage;
-	S_ys = S_ut*self.given.S_ys_percentage;
-	tau_s = K_s*8*F_s*self.C/pi/self.d^2;
-	SF_yield = S_ys/tau_s;
-	tau_a = K_w*8*(self.given.F_max-self.given.F_min)/2*self.C/pi/self.d^2;
-	tau_m = K_w*8*abs(self.given.F_max+self.given.F_min)*self.C/pi/self.d^2;
-	S_s = S_ut*self.given.S_s_percentage;
-	S_f = S_s/2/(1-S_s/2/S_us);
-	SF_fatigue = 1/(tau_a/S_f+tau_m/S_us);
 	file = printer('ME424_Project3_Gecgel_Sipahioglu.txt');
 	file.print('F_min = %g N', self.given.F_min);
 	file.print('F_max = %g N', self.given.F_max);
 	file.print('d = %g m', self.d);
 	file.print('C = %g', self.C);
-	file.print('D = %g m', D);
+	file.print('D = %g m', self.D);
 	file.print('delta_cl = %g m', 0.1*self.given.delta_max);
-	file.print('k = %g N/m', k);
-	file.print('N_a = %g', N_a);
+	file.print('k = %g N/m', self.k);
+	file.print('N_a = %g', self.N_a);
 	file.print('N_t = %g', self.N_t);
-	file.print('L_s = %g m', L_s);
-	file.print('L_f = %g m', L_f);
-	file.print('F_s = %g N', F_s);
-	file.print('K_s = %g', K_s);
-	file.print('K_w = %g', K_w);
-	file.print('L_f/D = %g', L_f/D);
-	file.print('delta_max/L_f = %g', self.given.delta_max/L_f);
-	file.print('SF_buckling = %g', SF_buckling);
-	file.print('m = %g kg', m);
-	file.print('f_n = %g Hz', f_n);
-	file.print('SF_surge = %g', SF_surge);
-	file.print('S_ut = %g Pa', S_ut);
-	file.print('S_us = %g Pa', S_us);
-	file.print('S_y = %g Pa', S_y);
-	file.print('S_ys = %g Pa', S_ys);
-	file.print('tau_s = %g Pa', tau_s);
-	file.print('SF_yield = %g', SF_yield);
-	file.print('tau_a = %g Pa', tau_a);
-	file.print('tau_m = %g Pa', tau_m);
-	file.print('S_s = %g Pa', S_s);
-	file.print('SF_fatigue = %g', SF_fatigue);
+	file.print('L_s = %g m', self.L_s);
+	file.print('L_f = %g m', self.L_f);
+	file.print('F_s = %g N', self.F_s);
+	file.print('K_s = %g', self.K_s);
+	file.print('K_w = %g', self.K_w);
+	file.print('L_f/D = %g', self.L_f/self.D);
+	file.print('delta_max/L_f = %g', self.given.delta_max/self.L_f);
+	file.print('SF_buckling = %g', self.SF_buckling);
+	file.print('m = %g kg', self.m);
+	file.print('f_n = %g Hz', self.f_n);
+	file.print('SF_surge = %g', self.SF_surge);
+	file.print('S_ut = %g Pa', self.S_ut);
+	file.print('S_us = %g Pa', self.S_us);
+	file.print('S_y = %g Pa', self.S_y);
+	file.print('S_ys = %g Pa', self.S_ys);
+	file.print('tau_s = %g Pa', self.tau_s);
+	file.print('SF_yield = %g', self.SF_yield);
+	file.print('tau_a = %g Pa', self.tau_a);
+	file.print('tau_m = %g Pa', self.tau_m);
+	file.print('S_s = %g Pa', self.S_s);
+	file.print('SF_fatigue = %g', self.SF_fatigue);
 	file.print('cost = %g ₺', self.cost);
 end
 end
